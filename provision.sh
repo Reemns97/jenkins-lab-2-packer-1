0a6b57cc4b22d7adeb060024e9e44114aec5d7c5
Reem Als <46527514+Reemns97@users.noreply.github.com>
2020-08-31 01:29:58 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -40,6 +40,17 @@ echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt
 echo "done"
 
 git clone https://github.com/Reemns97/k8s-jenkins
-cd /k8s-jenkins
-source jenkins.sh
+	    k3d cluster create labs \
+	    -p 80:80@loadbalancer \
+	    -p 443:443@loadbalancer \
+	    -p 30000-32767:30000-32767@server[0] \
+	    -v /etc/machine-id:/etc/machine-id:ro \
+	    -v /var/log/journal:/var/log/journal:ro \
+	    -v /var/run/docker.sock:/var/run/docker.sock \
+	    --agents 3
+kubectl apply -f jenkins.namespace.yaml -f jenkins.helm.yaml -f ingress.yaml
+WAIT=90
+echo "Sleeping for $WAIT"
+sleep $WAIT
+echo "Making progress"
 
